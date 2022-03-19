e61625294debacb1afc8eb5e99b857c951c87c81
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-03-19 11:13:51 +0200

    [init] project skeleton created

---
+++ src/main/java/com/javaee9/javaee9finalproject/JavaEe9FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javaee9.javaee9finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEe9FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEe9FinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaee9/javaee9finalproject/JavaEe9FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee9.javaee9finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEe9FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
