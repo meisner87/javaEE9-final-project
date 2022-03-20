7a4335ab627ba73535b0767c45943b95d27120bc
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-03-20 14:08:37 +0200

    [change] initial config - frontend part

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [WelcomeComponent]
