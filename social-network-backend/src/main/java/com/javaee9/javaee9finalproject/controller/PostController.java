540b92bec36c8c726fac6468e3bd98bd96a13853
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-03-31 18:47:21 +0300

    [change] using dto objects instead of entity for PostController

--- social-network-backend/src/main/java/com/javaee9/javaee9finalproject/controller/PostController.java
+++ social-network-backend/src/main/java/com/javaee9/javaee9finalproject/controller/PostController.java
@@ -1,6 +1,6 @@
 package com.javaee9.javaee9finalproject.controller;
 
-import com.javaee9.javaee9finalproject.entity.Post;
+import com.javaee9.javaee9finalproject.dto.PostDto;
 import com.javaee9.javaee9finalproject.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +23,7 @@ public class PostController {
     //readRecentPosts - NEWER EVER !!!
     // /posts?boundary = vs /posts/recent
     @GetMapping("/recent")
-    public List<Post> readRecentPosts() {
+    public List<PostDto> readRecentPosts() {
     log.info("reading recent posts");
     return postService.readRecentPosts();
     }
